5fb5df01c1a1a7561b158a64cf1e476c83ada9f7
srishtytakyar <51585827+srishtytakyar@users.noreply.github.com>
2021-01-16 14:21:15 +0530

    Updates

---
+++ precache-manifest.1484483f95249f4b5fd77010a23adeb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f235e43d3bf86a7639ffb4c22a9b18",
+    "url": "/myalgorithms/index.html"
+  },
+  {
+    "revision": "59070b98974f3386dfe7",
+    "url": "/myalgorithms/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "2282aee114009065ef2f",
+    "url": "/myalgorithms/static/css/main.0f5479ff.chunk.css"
+  },
+  {
+    "revision": "59070b98974f3386dfe7",
+    "url": "/myalgorithms/static/js/2.1dab4723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myalgorithms/static/js/2.1dab4723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2282aee114009065ef2f",
+    "url": "/myalgorithms/static/js/main.a4fa35f4.chunk.js"
+  },
+  {
+    "revision": "368d00e122806f3c87ea",
+    "url": "/myalgorithms/static/js/runtime-main.83c587d0.js"
+  }
+]);
